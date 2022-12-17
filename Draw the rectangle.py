6918b55a5ef5f578213e1b89d67f4e0eb8c60d3c
GIRISH DHUMAL <girishdhumal11499@gmail.com>
2022-12-18 00:10:53 +0530

    assignment commit

--- Draw the rectangle.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-img = cv2.imread(r"C:\Users\Vikas\Downloads\simple.jpg")
-cv2.rectangle(img,(0,0),(200,200),(0,0,255),thickness = cv2.FILLED)
-cv2.imshow("window",img)
-cv2.waitKey(0)
-cv2.distroyAllWindows()
--- imwrite function.py
+++
@@ -1,4 +0,0 @@
-import cv2
-img = cv2.imread('h.png')
-cv2.imwrite('save.jpg',img)
-print("file saved successfully.")
